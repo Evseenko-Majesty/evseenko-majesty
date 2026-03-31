677d6cff76654bd88d18d63f51fa7cda49c71866
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-03-31 11:25:55 +0800

    Delete server directory

--- server/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-router.post('/user', userController.getUser);
-router.get('/balance/:telegram_id', userController.getBalance);
-
-module.exports = router;
