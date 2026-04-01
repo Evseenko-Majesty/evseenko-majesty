39e9ebdfca37554ca254d0d48ec9ad4aa03d609a
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-01 12:56:32 +0800

    Create roles.js

---
+++ shared/constants/roles.js
@@ -0,0 +1,13 @@
+export const ROLES = {
+    CLIENT: 'client',
+    MASTER: 'master',
+    ADMIN: 'admin',
+    OWNER: 'owner'
+};
+
+export const ROLE_NAMES = {
+    [ROLES.CLIENT]: 'Клиент',
+    [ROLES.MASTER]: 'Мастер',
+    [ROLES.ADMIN]: 'Администратор',
+    [ROLES.OWNER]: 'Владелец'
+};
