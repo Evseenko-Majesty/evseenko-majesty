3ccbb398ee440784640de4820d5dc3bc477cda9d
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-07 06:44:26 +0800

    Create cors.js

---
+++ server/middleware/cors.js
@@ -0,0 +1,13 @@
+// ============================================
+// НАСТРОЙКА CORS (разрешаем запросы с любых источников)
+// ============================================
+module.exports = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+};
