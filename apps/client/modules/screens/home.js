74463527fa178d5945af3cafbca02e404da52a4f
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-07 01:00:50 +0300

    Create home.js

---
+++ apps/client/modules/screens/home.js
@@ -0,0 +1,9 @@
+export function renderHome() {
+    return `
+        <div class="container">
+            <div class="logo">👑</div>
+            <h1>Evseenko Majesty</h1>
+            <p>Добро пожаловать!</p>
+        </div>
+    `;
+}
