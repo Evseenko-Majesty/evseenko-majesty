0eeedc48ac44246885e465ea242231a77f0d36d9
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-10 00:12:50 +0800

    Update app.js

--- apps/client/modules/app.js
+++ apps/client/modules/app.js
@@ -12,17 +12,14 @@ class App {
       home: new HomeScreen(this)
     };
   }
-  
   navigateTo(screenName) {
     const screen = this.screens[screenName];
     this.root.innerHTML = '';
     this.root.appendChild(screen.getElement());
-    
     if (screen.onMount) {
       screen.onMount();
     }
   }
-  
   start() {
     this.navigateTo('splash');
   }
