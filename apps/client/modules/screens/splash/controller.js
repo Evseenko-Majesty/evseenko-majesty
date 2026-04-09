3a662f77b4721745e526cb991507f912aa13ceab
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-09 22:02:22 +0800

    Update controller.js

--- apps/client/modules/screens/splash/controller.js
+++ apps/client/modules/screens/splash/controller.js
@@ -1,7 +1,17 @@
 import { render } from './view.js';
 
 export class SplashScreen {
+  constructor(app) {
+    this.app = app;
+  }
+  
   getElement() {
     return render();
+  }
+  
+  onMount() {
+    setTimeout(() => {
+      this.app.navigateTo('home');
+    }, 2000);
   }
 }
