5d156daf713078b342261db893b398ca4c8bef8c
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-09 20:08:05 +0800

    Update view.js

--- apps/client/modules/screens/splash/view.js
+++ apps/client/modules/screens/splash/view.js
@@ -6,6 +6,11 @@ export function render() {
   h1.className = 'splash__title';
   h1.textContent = 'EVSEENKO MAJESTY';
   
+  const copyright = document.createElement('div');
+  copyright.className = 'splash__copyright';
+  copyright.textContent = '© 2026 Evseenko Majesty. Все права защищены.';
+  
   div.appendChild(h1);
+  div.appendChild(copyright);
   return div;
 }
