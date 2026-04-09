92e4bc2855ffbe8770e3bae877ecfe9ffcc175a6
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-09 10:22:25 +0300

    Update app.js

--- apps/client/modules/app.js
+++ apps/client/modules/app.js
@@ -6,6 +6,7 @@ import { renderSplash } from 'apps/client/modules/screens/splash/view.js';
 import { animateSplash } from 'apps/client/modules/screens/splash/controller.js';
 import { initTelegram } from '/shared/js/theme.js';
 import { initTelegram } from '/shared/js/telegram.js';
+import { initTelegram } from '/shared/js/auth.js';
 // Инициализация Telegram (раскрытие + тема)
 initTelegram();
 
