5a18603fc37fd8d99759dbd333351bdeeaf3f925
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-10 00:11:10 +0800

    Update telegram.js

--- shared/js/telegram.js
+++ shared/js/telegram.js
@@ -1,15 +1,11 @@
 export function initTelegram() {
   const tg = window.Telegram.WebApp;
-  
   tg.ready();
   tg.expand();
-  
   const theme = tg.colorScheme;
   document.documentElement.setAttribute('data-theme', theme);
-  
   tg.onEvent('themeChanged', () => {
     document.documentElement.setAttribute('data-theme', tg.colorScheme);
   });
-  
   return tg;
 }
