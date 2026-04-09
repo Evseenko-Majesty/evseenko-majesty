9df3d664e0ad1e6eff6e33f3c06f9445accffad1
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-10 00:14:00 +0800

    Update view.js

--- apps/client/modules/screens/home/view.js
+++ apps/client/modules/screens/home/view.js
@@ -1,9 +1,7 @@
 export function render() {
   const div = document.createElement('div');
-  
   const h1 = document.createElement('h1');
   h1.textContent = 'Главная';
-  
   div.appendChild(h1);
   return div;
 }
