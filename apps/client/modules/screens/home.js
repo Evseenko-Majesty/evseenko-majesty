2c2900a84e6c80807fcd300c608f8a01c84a91bb
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-09 14:50:41 +0800

    Delete apps/client/modules/screens/home.js

--- apps/client/modules/screens/home.js
+++
@@ -1,9 +0,0 @@
-export function renderHome() {
-    return `
-        <div class="container">
-            <div class="logo">👑</div>
-            <h1>Evseenko Majesty</h1>
-            <p>Добро пожаловать!</p>
-        </div>
-    `;
-}
