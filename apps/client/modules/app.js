0528ca891ab64166ddb1d96f02dae4c642372848
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-09 19:49:23 +0800

    Update app.js

--- apps/client/modules/app.js
+++ apps/client/modules/app.js
@@ -1,4 +1,7 @@
+import { initTelegram } from '/shared/js/telegram.js';
 import { SplashScreen } from './screens/splash/controller.js';
+
+const tg = initTelegram();
 
 const root = document.getElementById('root');
 const splash = new SplashScreen();
