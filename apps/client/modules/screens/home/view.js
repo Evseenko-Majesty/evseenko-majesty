a1a2873f6e3d7001897d18109e6060cd260e97e5
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-09 22:00:49 +0800

    Create view.js

---
+++ apps/client/modules/screens/home/view.js
@@ -0,0 +1,9 @@
+export function render() {
+  const div = document.createElement('div');
+  
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Главная';
+  
+  div.appendChild(h1);
+  return div;
+}
