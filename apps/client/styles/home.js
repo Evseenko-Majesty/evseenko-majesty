b1478b27735f0054f92e3f05117c773646e1724d
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-10 13:47:34 +0800

    Create home.js

---
+++ apps/client/styles/home.js
@@ -0,0 +1,12 @@
+.home {
+  min-height: 100vh;
+  background: var(--bg-color);
+  padding-bottom: 80px;
+}
+
+.home__header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px 16px 0 16px;
+}
