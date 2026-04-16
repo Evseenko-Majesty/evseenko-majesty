01cd005d3e2afa17299088733aaf26f2b5508ccb
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-16 15:33:22 +0800

    Update controller.js

--- apps/client/modules/screens/home/controller.js
+++ apps/client/modules/screens/home/controller.js
@@ -1,25 +1,18 @@
 import { render } from './view.js';
-import { hideBackButton } from '/shared/js/telegram.js';
 
 export class HomeScreen {
   constructor(app) {
     this.app = app;
-    this.currentTab = 'home';
   }
   
   getElement() {
-    return render(this.app.user, this.currentTab, (tab) => this.switchTab(tab));
-  }
-  
-  switchTab(tab) {
-    this.currentTab = tab;
-    const root = document.getElementById('root');
-    root.innerHTML = '';
-    root.appendChild(this.getElement());
-    hideBackButton(this.app.tg);
-  }
-  
-  onMount() {
-    hideBackButton(this.app.tg);
+    const div = render(this.app.user);
+    
+    const moreBtn = div.querySelector('.bottom-nav__item:last-child');
+    moreBtn.addEventListener('click', () => {
+      this.app.navigateTo('more');
+    });
+    
+    return div;
   }
 }
