bcec371e2c550fb0666434402a286c88e7ecccab
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-16 15:09:57 +0800

    Update controller.js

--- apps/client/modules/screens/home/controller.js
+++ apps/client/modules/screens/home/controller.js
@@ -3,8 +3,17 @@ import { render } from './view.js';
 export class HomeScreen {
   constructor(app) {
     this.app = app;
+    this.currentTab = 'home';
   }
+  
   getElement() {
-    return render(this.app.user);
+    return render(this.app.user, this.currentTab, (tab) => this.switchTab(tab));
+  }
+  
+  switchTab(tab) {
+    this.currentTab = tab;
+    const root = document.getElementById('root');
+    root.innerHTML = '';
+    root.appendChild(this.getElement());
   }
 }
