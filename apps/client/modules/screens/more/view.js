8128ae55f847c626be534265519eeb4b89286001
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-16 15:34:00 +0800

    Create view.js

---
+++ apps/client/modules/screens/more/view.js
@@ -0,0 +1,17 @@
+export function render() {
+  const div = document.createElement('div');
+  div.className = 'home';
+  
+  const content = document.createElement('div');
+  content.style.padding = '16px';
+  
+  const title = document.createElement('h2');
+  title.style.color = 'var(--text-color)';
+  title.style.marginBottom = '16px';
+  title.textContent = 'Ещё';
+  
+  content.appendChild(title);
+  div.appendChild(content);
+  
+  return div;
+}
