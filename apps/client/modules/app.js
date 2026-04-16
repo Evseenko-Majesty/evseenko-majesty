0f59b56524e6b5cab175e8c8696bde8c4bdf458f
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-16 15:26:58 +0800

    Update app.js

--- apps/client/modules/app.js
+++ apps/client/modules/app.js
@@ -1,4 +1,4 @@
-import { initTelegram } from '/shared/js/telegram.js';
+import { initTelegram, setupBackButton, hideBackButton } from '/shared/js/telegram.js';
 import { SplashScreen } from './screens/splash/controller.js';
 import { HomeScreen } from './screens/home/controller.js';
 
@@ -7,19 +7,47 @@ class App {
     this.tg = initTelegram();
     this.root = document.getElementById('root');
     this.user = null;
+    this.history = [];
     this.screens = {
       splash: new SplashScreen(this),
       home: new HomeScreen(this)
     };
   }
+  
   navigateTo(screenName) {
     const screen = this.screens[screenName];
     this.root.innerHTML = '';
     this.root.appendChild(screen.getElement());
+    
+    this.history.push(screenName);
+    
+    if (screenName === 'home') {
+      hideBackButton(this.tg);
+    } else {
+      setupBackButton(this.tg, () => this.goBack());
+    }
+    
     if (screen.onMount) {
       screen.onMount();
     }
   }
+  
+  goBack() {
+    if (this.history.length > 1) {
+      this.history.pop();
+      const previousScreen = this.history[this.history.length - 1];
+      const screen = this.screens[previousScreen];
+      this.root.innerHTML = '';
+      this.root.appendChild(screen.getElement());
+      
+      if (previousScreen === 'home') {
+        hideBackButton(this.tg);
+      }
+    } else {
+      this.tg.close();
+    }
+  }
+  
   start() {
     this.navigateTo('splash');
   }
