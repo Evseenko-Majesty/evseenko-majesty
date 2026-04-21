aaa7bbdfbb654f2320568cb84826281aee4b6d46
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-21 10:16:57 +0800

    Update view.js

--- apps/client/modules/screens/splash/view.js
+++ apps/client/modules/screens/splash/view.js
@@ -3,13 +3,19 @@
 // ============================================
 
 import { Logo } from '/shared/components/Logo.js';
+import { BrandName } from '/shared/components/BrandName.js';
 
 export function render() {
   const div = document.createElement('div');
   div.className = 'splash';
   
-  // Добавляем логотип
-  div.appendChild(Logo());
+  const brandBlock = document.createElement('div');
+  brandBlock.className = 'splash__brand';
+  
+  brandBlock.appendChild(Logo());
+  brandBlock.appendChild(BrandName());
+  
+  div.appendChild(brandBlock);
   
   return div;
 }
