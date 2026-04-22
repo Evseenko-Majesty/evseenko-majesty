8f729f6aad873bd441f2b2ccb980239f0892f43f
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-22 23:25:19 +0800

    Update telegram.js

--- shared/js/telegram.js
+++ shared/js/telegram.js
@@ -4,22 +4,15 @@
 
 export function initTelegram() {
   const tg = window.Telegram.WebApp;
+  
   tg.ready();
   tg.expand();
+  
   document.documentElement.setAttribute('data-theme', tg.colorScheme);
+  
   tg.onEvent('themeChanged', () => {
     document.documentElement.setAttribute('data-theme', tg.colorScheme);
   });
+  
   return tg;
-}
-
-// Просто показать кнопку и задать что делать
-export function setBackButton(tg, callback) {
-  tg.BackButton.show();
-  tg.BackButton.onClick(callback);
-}
-
-// Скрыть кнопку
-export function hideBackButton(tg) {
-  tg.BackButton.hide();
 }
