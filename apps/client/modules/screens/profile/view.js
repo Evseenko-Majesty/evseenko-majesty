6acc4a9add41ab5811585caedcffcfe20dc71382
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-22 21:54:31 +0800

    Update view.js

--- apps/client/modules/screens/profile/view.js
+++ apps/client/modules/screens/profile/view.js
@@ -3,6 +3,7 @@
 // ============================================
 
 import { Header } from '/shared/components/Header.js';
+import { UserInfo } from '/shared/components/UserInfo.js';
 
 export function render(user) {
   const div = document.createElement('div');
@@ -18,7 +19,11 @@ export function render(user) {
   title.className = 'page-title';
   title.textContent = 'Профиль';
   
+  const userInfo = UserInfo(user);
+  userInfo.classList.add('user-info--profile');
+  
   content.appendChild(title);
+  content.appendChild(userInfo);
   div.appendChild(content);
   
   return div;
