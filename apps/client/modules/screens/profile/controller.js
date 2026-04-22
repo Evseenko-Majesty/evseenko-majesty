bc38acc7e4d71e6238390737f8eb4a26e5016000
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-22 21:39:40 +0800

    Create controller.js

---
+++ apps/client/modules/screens/profile/controller.js
@@ -0,0 +1,15 @@
+// ============================================
+// СТРАНИЦА "ПРОФИЛЬ" — ЛОГИКА
+// ============================================
+
+import { render } from './view.js';
+
+export class ProfileScreen {
+  constructor(app) {
+    this.app = app;
+  }
+  
+  getElement() {
+    return render(this.app.user);
+  }
+}
