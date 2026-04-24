2d4360004cda2c0b97669bcb0226ab0f95982151
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-24 22:18:37 +0800

    Create supabase.js

---
+++ server/config/supabase.js
@@ -0,0 +1,13 @@
+// ============================================
+// ПОДКЛЮЧЕНИЕ К SUPABASE
+// ============================================
+
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
