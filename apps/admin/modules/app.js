e1d5898a6cfd92df89cee0d9f6dddc3a0fbb5b22
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-26 05:52:37 +0800

    Update app.js

--- apps/admin/modules/app.js
+++ apps/admin/modules/app.js
@@ -53,8 +53,8 @@ class AdminApp {
   
   navigateTo(screenName, fromBack = false, data = null) {
     // Если переход на grant-user с данными — создаём экран
-    if (screenName === 'grantuser' && data) {
-      this.screens['grantuser'] = new GrantUserScreen(this, data);
+    if (screenName === 'grant-user' && data) {
+      this.screens['grant-user'] = new GrantUserScreen(this, data);
     }
     
     if (!fromBack) {
