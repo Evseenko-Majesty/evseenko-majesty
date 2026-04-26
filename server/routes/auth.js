a68cb84ca8f660bf8be31701fead9e249799aac6
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-27 00:48:01 +0800

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,8 @@
-// ============================================
-// МАРШРУТЫ АВТОРИЗАЦИИ
-// ============================================
-
 import { Router } from 'express';
 import { authUser } from '../controllers/authController.js';
 
 const router = Router();
 
-// POST /api/auth
 router.post('/', authUser);
 
 export default router;
