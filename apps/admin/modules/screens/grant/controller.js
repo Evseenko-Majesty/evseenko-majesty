e66d40500416b930cba3318be79c9831371cd597
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-27 02:36:29 +0800

    Update controller.js

--- apps/admin/modules/screens/grant/controller.js
+++ apps/admin/modules/screens/grant/controller.js
@@ -1,11 +1,15 @@
 import { render } from './view.js';
+import { API } from '/shared/js/api.js';
 
 export class GrantScreen {
   constructor(app) {
     this.app = app;
   }
   
-  getElement() {
-    return render((screen) => this.app.navigateTo(screen));
+  async getElement() {
+    const res = await API.checkPermission(this.app.user?.telegram_id, 'grantForm');
+    const showGrantForm = res.hasAccess;
+    
+    return render((screen) => this.app.navigateTo(screen), showGrantForm);
   }
 }
