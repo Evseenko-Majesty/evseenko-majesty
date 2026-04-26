c8f5a5370d1bb2a6dd8712a8b3aa793205bf0936
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-26 23:07:11 +0800

    Update view.js

--- apps/admin/modules/screens/splash/view.js
+++ apps/admin/modules/screens/splash/view.js
@@ -5,6 +5,8 @@
 import { Logo } from '/shared/components/Logo.js';
 import { BrandName } from '/shared/components/BrandName.js';
 import { Slogan } from '/shared/components/Slogan.js';
+import { Copyright } from '/shared/components/Copyright.js';
+
 
 export function render() {
   const div = document.createElement('div');
@@ -13,6 +15,7 @@ export function render() {
   div.appendChild(Logo());
   div.appendChild(BrandName());
   div.appendChild(Slogan());
+div.appendChild(Copyright());
   
   return div;
  }
