80b9ea6b2a256e70b00f05be6a848806c906062f
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-26 15:42:21 +0800

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,13 +3,13 @@
 // ============================================
 
 import { Router } from 'express';
-import { searchUsers } from '../controllers/userController.js';
 import { searchUsers, getStaffUsers } from '../controllers/userController.js';
 
 const router = Router();
 
 // GET /api/users/search?query=...
 router.get('/search', searchUsers);
+
 // GET /api/users/staff
 router.get('/staff', getStaffUsers);
 
