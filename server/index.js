69360729090c554988a0b1940765155a773fe99f
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-27 00:44:31 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,24 +8,16 @@ import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import permissionRoutes from './routes/permissions.js';
 
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Проверка
-app.get('/', (req, res) => {
-  res.send('Evseenko Majesty Server OK');
-});
+app.get('/', (req, res) => res.send('Evseenko Majesty Server OK'));
 
-// Подключаем маршруты
 app.use('/api/auth', authRoutes);
-// После auth:
 app.use('/api/users', userRoutes);
-// После остальных маршрутов:
 app.use('/api/permissions', permissionRoutes);
-
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Сервер на порту ${PORT}`));
