87c4f64f875a035ab0dde3f0efdd80e74aa50090
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-27 00:39:11 +0800

    Update controller.js

--- apps/admin/modules/screens/more/controller.js
+++ apps/admin/modules/screens/more/controller.js
@@ -1,11 +1,15 @@
 import { render } from './view.js';
+import { API } from '/shared/js/api.js';
 
 export class MoreScreen {
   constructor(app) {
     this.app = app;
   }
   
-  getElement() {
-    return render(this.app.user, (screen) => this.app.navigateTo(screen));
+  async getElement() {
+    const res = await API.checkPermission(this.app.user?.telegram_id, 'grant');
+    const showGrant = res.hasAccess;
+    
+    return render(this.app.user, (screen) => this.app.navigateTo(screen), showGrant);
   }
 }
