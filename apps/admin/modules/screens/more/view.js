88910085c4b8e35ee6a5c3c2c8938bae1bb7388f
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-26 21:24:00 +0800

    Update view.js

--- apps/admin/modules/screens/more/view.js
+++ apps/admin/modules/screens/more/view.js
@@ -32,26 +32,9 @@ export function render(user, onNavigate) {
     <span class="menu-item__arrow">›</span>
   `;
   profileItem.addEventListener('click', () => onNavigate('profile'));
-
-  // Дать доступ
-const grantItem = document.createElement('div');
-grantItem.className = 'menu-item';
-grantItem.innerHTML = `
-  <span class="menu-item__icon">
-    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-      <path d="M12 2L3 7v6c0 5.25 3.83 10.15 9 11c5.17 -0.85 9 -5.75 9 -11V7L12 2z"/>
-      <path d="M9 12l2 2l4 -4"/>
-    </svg>
-  </span>
-  <span>Дать доступ</span>
-  <span class="menu-item__arrow">›</span>
-`;
-grantItem.addEventListener('click', () => onNavigate('grant'));
-
   
   content.appendChild(title);
   content.appendChild(profileItem);
-  content.appendChild(grantItem);
   div.appendChild(content);
   
   return div;
