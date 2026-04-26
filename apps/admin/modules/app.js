cddf4724a00a583b59e7d2e782f4eb6001d577a8
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-27 02:27:24 +0800

    Update app.js

--- apps/admin/modules/app.js
+++ apps/admin/modules/app.js
@@ -9,7 +9,7 @@ import { MoreScreen } from './screens/more/controller.js';
 import { ProfileScreen } from './screens/profile/controller.js';
 import { BottomNav } from '/shared/components/BottomNav.js';
 import { GrantScreen } from './screens/grant/controller.js';
-
+import { GrantFormScreen } from './screens/grant-form/controller.js';
 
 class AdminApp {
   constructor() {
@@ -23,7 +23,8 @@ class AdminApp {
       home: new HomeScreen(this),
       more: new MoreScreen(this),
       profile: new ProfileScreen(this),
-      grant: new GrantScreen(this)
+      grant: new GrantScreen(this),
+      grantForm: new GrantFormScreen(this)
     };
     
     this.navItems = [
