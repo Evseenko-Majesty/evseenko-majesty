42c0a5d7df20083262f06619e950a80994756ea6
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-27 07:07:42 +0800

    Update app.js

--- apps/admin/modules/app.js
+++ apps/admin/modules/app.js
@@ -7,9 +7,10 @@ import { SplashScreen } from './screens/splash/controller.js';
 import { HomeScreen } from './screens/home/controller.js';
 import { MoreScreen } from './screens/more/controller.js';
 import { ProfileScreen } from './screens/profile/controller.js';
-import { BottomNav } from '/shared/components/BottomNav.js';
 import { GrantScreen } from './screens/grant/controller.js';
 import { GrantFormScreen } from './screens/grant-form/controller.js';
+import { GrantUserScreen } from './screens/grant-user/controller.js';
+import { BottomNav } from '/shared/components/BottomNav.js';
 
 class AdminApp {
   constructor() {
@@ -33,39 +34,42 @@ class AdminApp {
     ];
     
     this.tg.BackButton.onClick(() => {
-  if (this.screenHistory.length > 1) {
-    this.screenHistory.pop();
-    const prev = this.screenHistory[this.screenHistory.length - 1];
-    this.navigateTo(prev, true);
-  }
-});
-  }
-  async navigateTo(screenName, fromBack = false) {
-  if (!fromBack) this.screenHistory.push(screenName);
-  
-  const screen = this.screens[screenName];
-  if (!screen) return;
-  
-  // Сначала получаем новый контент
-  const element = await screen.getElement();
-  
-  // Потом очищаем и вставляем (без мигания)
-  this.container.innerHTML = '';
-  this.container.appendChild(element);
-  
-  if (screenName === 'home' || screenName === 'splash') {
-    this.tg.BackButton.hide();
-  } else {
-    this.tg.BackButton.show();
+      if (this.screenHistory.length > 1) {
+        this.screenHistory.pop();
+        const prev = this.screenHistory[this.screenHistory.length - 1];
+        this.navigateTo(prev, true);
+      }
+    });
   }
   
-  if (screenName === 'home' || screenName === 'more') {
-    this.container.appendChild(BottomNav(this.navItems, screenName, (id) => this.navigateTo(id)));
+  async navigateTo(screenName, fromBack = false, data = null) {
+    // Динамический экран выдачи роли
+    if (screenName === 'grant-user' && data) {
+      this.screens['grant-user'] = new GrantUserScreen(this, data);
+    }
+    
+    if (!fromBack) this.screenHistory.push(screenName);
+    
+    const screen = this.screens[screenName];
+    if (!screen) return;
+    
+    const element = await screen.getElement();
+    
+    this.container.innerHTML = '';
+    this.container.appendChild(element);
+    
+    if (screenName === 'home' || screenName === 'splash') {
+      this.tg.BackButton.hide();
+    } else {
+      this.tg.BackButton.show();
+    }
+    
+    if (screenName === 'home' || screenName === 'more') {
+      this.container.appendChild(BottomNav(this.navItems, screenName, (id) => this.navigateTo(id)));
+    }
+    
+    if (screen.onMount) screen.onMount();
   }
-  
-  if (screen.onMount) screen.onMount();
-}
-  
   
   start() {
     this.navigateTo('splash');
