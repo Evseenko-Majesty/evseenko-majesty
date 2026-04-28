180a80a5eb59a8277e61fb1704fa75745f4cfc13
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-04-29 04:31:45 +0800

    Update permissions.js

--- server/routes/permissions.js
+++ server/routes/permissions.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { checkPermission, checkPermissionsByType, updateUserRole } from '../controllers/permissionController.js';
+import { updateUserPosition } from '../controllers/permissionController.js';
 
 const router = Router();
 
 router.get('/check', checkPermission);
 router.get('/check-type', checkPermissionsByType);
 router.put('/role', updateUserRole);
+router.put('/position', updateUserPosition);
 
 export default router;
