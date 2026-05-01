b1be39fd4897143e9475a1c017a5415e69cf4d8f
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-05-01 19:46:00 +0800

    Update app.js

--- apps/client/modules/app.js
+++ apps/client/modules/app.js
@@ -6,14 +6,34 @@ import { initTelegram } from '/shared/js/telegram.js';
 import { SplashScreen } from './screens/splash/controller.js';
 import { HomeScreen } from './screens/home/controller.js';
 
-const tg = initTelegram();
-const container = document.getElementById('app');
+class App {
+  constructor() {
+    this.tg = initTelegram();
+    this.container = document.getElementById('app');
+    
+    // Все экраны приложения
+    this.screens = {
+      splash: new SplashScreen(this),
+      home: new HomeScreen(this)
+    };
+  }
+  
+  // Переход на другой экран
+  navigateTo(screenName) {
+    const screen = this.screens[screenName];
+    this.container.innerHTML = '';                    // Очищаем контейнер
+    this.container.appendChild(screen.getElement());  // Вставляем новый экран
+    
+    if (screen.onMount) {
+      screen.onMount();
+    }
+  }
+  
+  // Запуск приложения
+  start() {
+    this.navigateTo('splash');
+  }
+}
 
-// Все экраны
-const screens = {
-  splash: new SplashScreen(null),
-  home: new HomeScreen(null)
-};
-
-// Показываем загрузочный
-container.appendChild(screens.splash.getElement());
+const app = new App();
+app.start();
