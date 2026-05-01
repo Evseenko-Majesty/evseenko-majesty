30b35eacd2ee562836bea1171395d0fc8f3593c2
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-05-01 17:14:10 +0800

    Delete server directory

--- server/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { authUser } from '../controllers/authController.js';
-
-const router = Router();
-
-router.post('/', authUser);
-
-export default router;
--- server/routes/users.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { searchUsers, getVisibleUsers } from '../controllers/userController.js';
-
-const router = Router();
-
-router.get('/search', searchUsers);
-router.get('/visible', getVisibleUsers);
-
-export default router;
