9e1a575f9b00ea5c0bf29f8829e18705dd3c7e92
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-05-02 12:33:59 +0800

    Update view.js

--- apps/client/modules/screens/home/view.js
+++ apps/client/modules/screens/home/view.js
@@ -3,9 +3,6 @@ import { Header } from '/shared/components/Header.js';
 export function render(user) {
   const div = document.createElement('div');
   div.className = 'home';
-  
-  // Шапка (логотип слева, пока без правого контента)
   div.appendChild(Header(null));
-  
   return div;
 }
