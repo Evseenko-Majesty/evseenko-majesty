914a41cf0f4e3cb279dfe561ec9a9af276bd0b1a
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-05-02 12:33:31 +0800

    Update view.js

--- apps/client/modules/screens/splash/view.js
+++ apps/client/modules/screens/splash/view.js
@@ -6,11 +6,9 @@ import { Copyright } from '/shared/components/Copyright.js';
 export function render() {
   const div = document.createElement('div');
   div.className = 'splash';
-  
   div.appendChild(Logo());
   div.appendChild(BrandName());
   div.appendChild(Slogan());
   div.appendChild(Copyright());
-  
   return div;
 }
