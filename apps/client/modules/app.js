d482d011bc5f10c83e547c7df3bf8bb1c4411bc7
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-05-02 17:50:35 +0800

    Update app.js

--- apps/client/modules/app.js
+++ apps/client/modules/app.js
@@ -1,6 +1,8 @@
 import { initTelegram } from '/shared/js/telegram.js';
 import { SplashScreen } from './screens/splash/controller.js';
 import { HomeScreen } from './screens/home/controller.js';
+import { MoreScreen } from './screens/more/controller.js';
+import { BottomNav } from '/shared/components/BottomNav.js';
 
 class App {
   constructor() {
@@ -10,16 +12,23 @@ class App {
     
     this.screens = {
       splash: new SplashScreen(this),
-      home: new HomeScreen(this)
+      home: new HomeScreen(this),
+      more: new MoreScreen(this)
     };
   }
+  this.navItems = [
+  { id: 'home', label: 'Главная', icon: '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M5 21h14M5 21v-13M19 21v-13M9 21v-8h6v8M2 10l10-8 10 8"/></svg>' },
+  { id: 'more', label: 'Ещё', icon: '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M5 5h14M5 12h14M5 19h14"/></svg>' }
+];
   
   navigateTo(screenName) {
     const screen = this.screens[screenName];
     this.container.innerHTML = '';
     this.container.appendChild(screen.getElement());
     if (screen.onMount) screen.onMount();
-  }
+   if (screenName === 'home' || screenName === 'more') {
+  this.container.appendChild(BottomNav(this.navItems, screenName, (id) => this.navigateTo(id)));
+}
   
   start() {
     this.navigateTo('splash');
