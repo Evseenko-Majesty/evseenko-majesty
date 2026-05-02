94dd37a6afac4f4c74bc8c58075b65e6abf92b31
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-05-02 12:34:29 +0800

    Update controller.js

--- apps/client/modules/screens/home/controller.js
+++ apps/client/modules/screens/home/controller.js
@@ -1,11 +1,6 @@
 import { render } from './view.js';
 
 export class HomeScreen {
-  constructor(app) {
-    this.app = app;
-  }
-  
-  getElement() {
-    return render(this.app.user);
-  }
+  constructor(app) { this.app = app; }
+  getElement() { return render(this.app.user); }
 }
