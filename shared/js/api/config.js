97fae6429dfd223413370092562a6a5438336921
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-05-02 20:35:29 +0300

    Update API_URL to new server address

--- shared/js/api/config.js
+++ shared/js/api/config.js
@@ -1,2 +1,2 @@
-// Адрес сервера — один раз, используется во всех API-файлах aaa
-export const API_URL = 'https://evseenko-api-test.onrender.com';
+// Адрес сервера — один раз, используется во всех API-файлах
+export const API_URL = 'https://evseenko-api.onrender.com';
