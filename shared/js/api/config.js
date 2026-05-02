072e7e0887525df5327018c1f55bd07ee60125d6
Evseenko Majesty <evseenko_majesty@mail.ru>
2026-05-02 18:45:04 +0300

    gh

--- shared/js/api/config.js
+++ shared/js/api/config.js
@@ -1,2 +1,2 @@
-// Адрес сервера — один раз, используется во всех API-файлах
+// Адрес сервера — один раз, используется во всех API-файлах aaa
 export const API_URL = 'https://evseenko-api-test.onrender.com';
