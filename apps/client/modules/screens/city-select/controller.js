60c070cdcbdddb74528a611a2b505600de700dbb
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-05-05 19:15:24 +0800

    Update controller.js

--- apps/client/modules/screens/city-select/controller.js
+++ apps/client/modules/screens/city-select/controller.js
@@ -4,8 +4,8 @@ import { Modal } from '/shared/components/Modal.js';
 export class CitySelectScreen {
   constructor(app) { this.app = app; }
   
-  getElement() {
-    return render((city) => {
+  async getElement() {
+    return await render((city) => {
       localStorage.setItem('selectedCity', JSON.stringify(city));
       document.body.appendChild(Modal('Готово', `Город "${city.name}" сохранён`, 'Ок', () => {
         this.app.navigateTo('profile');
