ebb35824b1f1650912d3868ffe188d9cb5643c9f
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-05-05 19:07:44 +0800

    Update controller.js

--- apps/client/modules/screens/city-select/controller.js
+++ apps/client/modules/screens/city-select/controller.js
@@ -1,6 +1,15 @@
 import { render } from './view.js';
+import { Modal } from '/shared/components/Modal.js';
 
 export class CitySelectScreen {
   constructor(app) { this.app = app; }
-  getElement() { return render(); }
+  
+  getElement() {
+    return render((city) => {
+      localStorage.setItem('selectedCity', JSON.stringify(city));
+      document.body.appendChild(Modal('Готово', `Город "${city.name}" сохранён`, 'Ок', () => {
+        this.app.navigateTo('profile');
+      }));
+    });
+  }
 }
