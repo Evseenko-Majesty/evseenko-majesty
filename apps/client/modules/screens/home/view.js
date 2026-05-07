8e82bfe8057d6b8fd30a72f816a71209f78adf9c
Evseenko-Majesty <prince_evseenko@mail.ru>
2026-05-07 19:04:33 +0800

    Update view.js

--- apps/client/modules/screens/home/view.js
+++ apps/client/modules/screens/home/view.js
@@ -1,9 +1,12 @@
 import { Header } from '/shared/components/Header.js';
 import { UserInfo } from '/shared/components/UserInfo.js';
+import { CityLabel } from '/shared/components/CityLabel.js';
 
-export function render(user, onProfileClick) {
+export function render(user, onProfileClick, onCityClick, cityName) {
   const div = document.createElement('div');
   div.className = 'home';
+  
+  div.appendChild(CityLabel(cityName, onCityClick));
   
   const userInfo = UserInfo(user, { showUsername: true });
   userInfo.style.cursor = 'pointer';
